43a2b1c5542675b68e6175090e2fc7ad6858c7a6
mdabuzafar <mdabu.zafar@vista.com>
2025-12-09 11:32:18 +0530

    refactor: rename URL constant to API_URL for clarity

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-export const URL = `https://omdbapi.com/?apikey=${API_KEY}`;
+export const API_URL = `https://omdbapi.com/?apikey=${API_KEY}`;
